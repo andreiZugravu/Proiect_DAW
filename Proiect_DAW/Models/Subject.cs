761a42efd2a7c7cd9382a42d73b469de6831f48a
Andrei <andrei.zugravu97@gmail.com>
2018-12-28 14:15:58 +0200

    add subject and category models

---
+++ Proiect_DAW/Models/Subject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Proiect_DAW.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int SubjectId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Data { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+
+    public class SubjectDBContext : DbContext
+    {
+        public SubjectDBContext() : base("DBConnectionString") { }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
